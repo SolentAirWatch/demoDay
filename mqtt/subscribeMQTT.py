83866986ea56078f0228d0d447bf5550ba075091
joshctaylor <joshua.taylor312@gmail.com>
2017-06-08 00:42:28 +0100

    fixed 100 message problem

--- mqtt/subscribeMQTT.py
+++ mqtt/subscribeMQTT.py
@@ -1,37 +1,45 @@
+#    this script forms part of solent air watch data back end
+#    it subcribes to an MQTT broker and adds the data to a sqlite database
+#    based on example code from https://github.com/eclipse/paho.mqtt.python/tree/master/examples
+
+#    Copyright (C) {2017}  {Joshua Taylor - Solent Air Watch}
+
 import paho.mqtt.client as mqtt
 import json
 import sqlite3
-from pprint import pprint # makes data more pretty
+from pprint import pprint  # makes data more pretty
 
-# broker = "46.101.13.195" # this is our cloud server used to isolate any issues with opensenors which is more complex
-broker = "mqtt.opensensors.io"
-DB_Name =  "airwatchData.db"
-global cursor
+global cursor  # make the database handle available globally
+broker = "mqtt.opensensors.io"  # digital oceans IP is "46.101.13.195"
+DB_Name = "airwatchData.db"
+topicName = "/orgs/solentairwatch/sniffy"
 
-## this section is based on code from https://eclipse.org/paho/clients/python
+
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, rc):
     print("Connected with result code "+str(rc))
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("/orgs/solentairwatch/sniffy", qos=0)
+    client.subscribe(topicName, qos=0)
+
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     data = json.loads(msg.payload.decode('utf-8'))
     pprint(data)
-    # parse the data to the sql database, prob a way to do directly from JSON, this works though
+    # parse the data to the sql database
     cursor.execute('''INSERT INTO sniffy(id, timestmp, latitude, longitude, PM10, PM25, PM1)
-                  VALUES(?,?,?,?,?,?,?)''', (data["id"], data["time"], data["latitude"], data["longitude"], data["PM10"], data["PM25"], data["PM1"]))
+                  VALUES(?,?,?,?,?,?,?)''', (data["id"], data["time"], data["latitude"],
+                                             data["longitude"], data["PM10"], data["PM25"], data["PM1"]))
     db.commit()
-    
+
 client = mqtt.Client(client_id="6423")
 client.username_pw_set("solentairwatch", password="aLmgqJPH")
 
 # set MQTT call back functions
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect(broker) # (address, port, timeout (sec) )
+client.connect(broker)  # (address, port, timeout (sec) )
 
 # set up database connection
 db = sqlite3.connect(DB_Name)
@@ -43,7 +51,5 @@ cursor.execute('''
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
 client.loop_forever()
-db.close() # this will never be executed because of the forever loop - need some exit logic
+db.close()  # this will never be executed because of the forever loop - need some exit logic
--- mqtt/testData.py
+++ mqtt/testData.py
@@ -1,58 +1,59 @@
+#    A script to send dummy Sniffy data to the MQTT broker
+
 import paho.mqtt.client as mqtt
 import numpy as np
 import json
 import time
 import datetime
 
-## dont run for too long as the time index is unbound - memory leaks ahoy!!!
 
-#client.username_pw_set("solentairwatch", password="YfLNeHZF")
-broker = "mqtt.opensensors.io"
+# dont run for too long as the time index is unbound - memory leaks ahoy!!!
+broker = "mqtt.opensensors.io"  # "46.101.13.195"     # test broker
 topic = "/orgs/solentairwatch/sniffy"
-# broker  = "46.101.13.195" # test broker
-monitorID = 1
+monitorID = 'SOTON0000'  # id 0 is reserved for test
 monitorLocation = [50.9262, -1.4092]
 global t
 global ts
 
+
 def on_connect(client, userdata, rc):
     print("Connected with result code "+str(rc))
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
+    # do nothing we're connected
     pass
-#          ]
-#       }
 
 
 def on_publish(client, userdata, mid):
     print(mid)
-    print('published a message')   	
+    print('published a message')
     pass
-        
+
+
 def on_disconnect(client, userdata, rc):
-    print('disconected with code' +str(rc))
-        
+    # need some ability to error log and reconnect
+    print('disconected with code' + str(rc))
+
 client = mqtt.Client(client_id="6435")
 client.username_pw_set("solentairwatch", password="IFwwJ6vO")
 
 # set MQTT call back functions
 client.on_connect = on_connect
 client.on_publish = on_publish
-client.connect(broker) # (address, port, timeout (sec) )
+client.connect(broker)  # (address, port, timeout (sec) )
 t = 0
 ts = 1
 client.loop()
 
 while True:
-    x = np.random.randn()#20 + (20*np.sin(2*np.pi*t*0.01)) # generate a 0.01Hz sinewave between 0 and 40
+    x = 20 + (20 * np.sin(2 * np.pi * t * 0.01))  # generate a sinewave
+    t = t + ts  # time update
     time.sleep(1)
     message = {
-            'id': 'SOTON0001',
+            'id': monitorID,
             'cityName': "Southampton",
             'stationName': "Common#1",
             'latitude': monitorLocation[0],
             'longitude': monitorLocation[1],
-#          'pollutants': [
+# 'pollutants': [
             'PM10': 0,
             'PM1': 0,
             'PM25': x,
@@ -61,6 +62,4 @@ while True:
         }
     client.publish(topic, payload=json.dumps(message), qos=0, retain=False)
     client.loop()
-#print(json.dumps(message))
-     
-    
+# print(json.dumps(message))
