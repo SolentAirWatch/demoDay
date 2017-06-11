ea57e70fc09ae6516456c1c296717aefa6d50f7b
joshctaylor <joshua.taylor312@gmail.com>
2017-06-11 20:03:26 +0100

    mod testData for 2 senors, add install script

--- mqtt/testData.py
+++ mqtt/testData.py
@@ -1,4 +1,4 @@
-#    A script to send dummy Sniffy data to the MQTT broker
+#    A script to send dummy Sniffy data to the MQTT broker - tested on python 3
 
 import paho.mqtt.client as mqtt
 import numpy as np
@@ -10,7 +10,8 @@ import datetime
 # dont run for too long as the time index is unbound - memory leaks ahoy!!!
 broker = "mqtt.opensensors.io"  # "46.101.13.195"     # test broker
 topic = "/orgs/solentairwatch/sniffy"
-monitorID = 'SOTON0000'  # id 0 is reserved for test
+monitorID1 = '0'  # id 0 is reserved for test
+monitorID2 = '1'  # id 0 is reserved for test
 monitorLocation = [50.9262, -1.4092]
 global t
 global ts
@@ -44,11 +45,12 @@ ts = 1
 client.loop()
 
 while True:
-    x = 20 + (20 * np.sin(2 * np.pi * t * 0.01))  # generate a sinewave
+    x1 = 20 + (100 * np.sin(2 * np.pi * t * 0.1))  # generate a sinewave
+    x2 = 20 + (100 * np.sin(2 * np.pi * t * 0.01))  # generate a sinewave
     t = t + ts  # time update
     time.sleep(1)
-    message = {
-            'id': monitorID,
+    message1 = {
+            'id': monitorID1,
             'cityName': "Southampton",
             'stationName': "Common#1",
             'latitude': monitorLocation[0],
@@ -56,10 +58,24 @@ while True:
 # 'pollutants': [
             'PM10': 0,
             'PM1': 0,
-            'PM25': x,
+            'PM25': x1,
             'time': str(datetime.datetime.now()),
             'averaging': 0
         }
-    client.publish(topic, payload=json.dumps(message), qos=0, retain=False)
+    message2 = {
+            'id': monitorID2,
+            'cityName': "Southampton",
+            'stationName': "Common#1",
+            'latitude': monitorLocation[0],
+            'longitude': monitorLocation[1],
+# 'pollutants': [
+            'PM10': 0,
+            'PM1': 0,
+            'PM25': x2,
+            'time': str(datetime.datetime.now()),
+            'averaging': 0
+        }
+    client.publish(topic, payload=json.dumps(message1), qos=0, retain=False)
+    client.publish(topic, payload=json.dumps(message2), qos=0, retain=False)
     client.loop()
 # print(json.dumps(message))
