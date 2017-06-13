1ee44c3c4de0cde262d70d680513ee092695a412
HD <asonedoes@gmail.com>
2017-06-13 04:05:51 +0100

    installed nodejs and website working

---
+++ NCAD/server/server.js
@@ -0,0 +1,26 @@
+var http = require("http");
+var connect = require('connect');
+var serveStatic = require('serve-static');
+
+//connect().use(serveStatic(__dirname)).listen(8080, function(){
+    //console.log('Server running on 8080...');
+//});
+
+console.log('\n\n--- Node Version: ' + process.version + ' ---');
+
+// Set up Connect routing
+var app = connect()
+    .use(serveStatic(__dirname + '/public'))
+    .use(function(req, res) {
+        console.log('Could not find handler for: ' + req.url);
+        res.end('Could not find handler for: ' + req.url);
+    })
+    .use(function(err, req, res, next) {
+        console.log('Error trapped by Connect: ' + err.message + ' : ' + err.stack);
+        res.end('Error trapped by Connect: ' + err.message);
+    });
+
+// Start node server listening on specified port -----
+http.createServer(app).listen(8081);
+
+console.log('HTTP server listening on port 8081');
