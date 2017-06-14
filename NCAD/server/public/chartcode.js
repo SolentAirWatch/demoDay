0f9d89503c7b00aacc0d25da4bcf194914dae0f0
HD <asonedoes@gmail.com>
2017-06-14 11:51:48 +0100

    now gets historical data correctly

--- NCAD/server/public/chartcode.js
+++ NCAD/server/public/chartcode.js
@@ -1,4 +1,4 @@
-var maxDataPointsInCharts = 100;
+var maxDataPointsInCharts = 1000;
 // this is a list of charts.
 var pmChartBySid = {};
 
@@ -19,14 +19,14 @@ function make_chart(id) {
 				xAxes: [{
 					type: 'time',
 					ticks: {
-						autoSkip: false,
-						maxRotation: 0,
-						minRotation: 0
+						autoSkip: true,
+						maxRotation: 50,
+						minRotation: 50
 					},
 					// position: 'bottom',
 					time: {
 						displayFormats: {
-							'millisecond': 'HH:MM:SS',
+							//'millisecond': 'HH:MM:SS',
 							'second': 'HH:MM:SS',
 							'minute': 'HH:MM:SS',
 							'hour': 'HH:MM:SS',
@@ -58,46 +58,47 @@ function make_chart(id) {
 }
 //ctx potentially interferes with the chart working, it's a variable used by Chart.js
 var ctx = undefined;
-window.addEventListener("load", function() {
-	// make pm charts by sid
-	var chartsContainer = document.getElementById("chart_container");
+//window.addEventListener("load", function() {
+// make pm charts by sid
+var chartsContainer = document.getElementById("chart_container");
 
-	//each chart gets a div with text etc
-	//for loop here is replaced so only get one copy
+//each chart gets a div with text etc
+//for loop here is replaced so only get one copy
 
-	for (var i=1;i<=1;i++) {
-		var el = document.createElement("div");
-		el.className="chart";
+for (var i=1;i<=1;i++) {
+	var el = document.createElement("div");
+	el.className="chart";
 
-		// sensor ID text
-		var txt = document.createElement("div");
-		txt.className = "chart_text";
-		txt.innerText = "Live sensor data";
-		el.appendChild(txt);
+	// sensor ID text
+	var txt = document.createElement("div");
+	txt.className = "chart_text";
+//	txt.innerText = "Live sensor data";
+	el.appendChild(txt);
 
-		// canvas
-		var canvas = document.createElement("canvas");
-		el.appendChild(canvas);
-		canvas.className="chart_canvas";
-		canvas.id = "chart_canvas";
-		chartsContainer.appendChild(el);
+	// canvas
+	var canvas = document.createElement("canvas");
+	el.appendChild(canvas);
+	canvas.className="chart_canvas";
+	canvas.id = "chart_canvas";
+	chartsContainer.appendChild(el);
+}
+//add a single chart for now
+for (var i=1;i<=1;i++) {
+	pmChartBySid[i] = {
+		"chart": make_chart("chart_canvas"),
+		"domid": "chart_canvas",
+		"dataset": null,
+		"timeout": null,
+		"name": "PM",
+		"IDbuffer": []
 	}
-	//add a single chart for now
-	for (var i=1;i<=1;i++) {
-		pmChartBySid[i] = {
-			"chart": make_chart("chart_canvas"),
-			"domid": "chart_canvas",
-			"dataset": null,
-			"timeout": null,
-			"name": "PM",
-			"IDbuffer": []
-		}
-	}
-});
+}
+//});
 
 //set up the event listener
 //so that we know when we first receive data
 var token ="";
+var password = "";
 var eventnum = 1;
 $.postJSON = function(url, data, callback) {
 	return jQuery.ajax({
@@ -135,56 +136,130 @@ while (!["H","L","h","l"].includes(plot_mode)) {
 	plot_mode = prompt("Plot (H)istorical or (L)ive data?", "L");
 }
 
+var APIkey = "c26fa96c-16b8-4fe9-967d-bf0e14047f38";
+
+var OpenSensorsUrl = "https://api.opensensors.io";
+var apiBaseUrl = "/v1/messages/topic//orgs/solentairwatch/sniffy?";
+//an array of all datapoints.
+var all_data = [];
 //decide which plotting mode
 //and then process data
 if(plot_mode == "H" || plot_mode == "h") {
 	//plotting historical data
-	var startDate = "2017-06-13T08:00:00Z";
-	var endDate = "2017-06-13T08:01:00Z";
-	var apiBaseUrl = "https://api.opensensors.io/v1/messages/topic//orgs/solentairwatch/sniffy?";
-	var apiHistoryUrl = apiBaseUrl + "start-date=" + startDate + "&end-date=" + endDate;
-	$.APIgetJSON(apiHistoryUrl, "58817278-964d-46de-bb15-bddfbfb6f2e6", function(data) {
+	var startDate = "2017-05-13T02:00:00Z";
+	var endDate = "2017-06-13T02:10:00Z";
+	getHistoricalData(startDate, endDate, true, onDataFinish);
+	/*
+	$.APIgetJSON(apiHistoryUrl, APIkey, function(data) {
+		var messageLength = data.messages.length;	
+		var message = {};
+		console.log(data);
+		var sid = 0;
+		console.log("got " + messageLength + " messages in total");
+	});
+*/
+} else {
+	/*
+	var realtimeAPIUrl = "https://realtime.opensensors.io/v1/events/topics//orgs/solentairwatch/sniffy";
+	console.log("making API request");
+	$.APIgetJSON(realtimeAPIUrl, APIkey, function(data, status) {
+		console.log("data received");
+		console.log("status: " + status);
+		console.log(data);
+	});
+	*/
+	//probably a bad idea...
+	var password = prompt("Enter opensensors password:", "");
+	//get the token, and on response set up event listener
+	setupLiveTokenListener();
+}
+
+function getHistoricalData(startDate, endDate, getSpoof, callback) {
+	//if getSpoof is false we ignore sids which are negative
+	//opensensors won't send all data at once, it will give url to next one.
+	var apiHistoryUrl = OpenSensorsUrl + apiBaseUrl + "start-date=" + startDate + "&end-date=" + endDate;
+	getJSONdata(apiHistoryUrl, getSpoof, callback);
+}
+
+function getJSONdata(url, getSpoof, callback) {
+	//console.log("JSON call here with callback :" + callback);
+	//recursive call to get all data
+	$.APIgetJSON(url, APIkey, function(data) {
 		var messageLength = data.messages.length;	
 		var message = {};
 		var sid = 0;
 		console.log("got " + messageLength + " messages in total");
-		for (var c = 0; c < messageLength; c++){
+		var c = 0;
+		for (c = 0; c < messageLength; c++){
 			message = JSON.parse(data.messages[c].payload.text);
-			sid = message.id;
-			console.log(message);
-			console.log("reading history item with sid = " + sid);
-			if ( c == 0) {
-				processInitialDataChart("pm", 1, message, sid);
-			} else {
-				processStreamDataChart("pm", message, sid);
+			//sid = message.id;
+			//console.log(message);
+			//console.log("reading history item with sid = " + sid);
+			if ( sid >= 0 || getSpoof == true ) {
+				all_data.push(message);
 			}
 		}
+		//console.log("next here is :" + data.next);
+		if (data.next === undefined ){
+			callback();
+			return;
+		} else {
+			console.log("getting more data!");
+			var newUrl = OpenSensorsUrl + data.next;
+			//console.log("new url is :" + newUrl);
+			getJSONdata(newUrl, getSpoof, callback);
+		}
 	});
-} else {
-	//get the token, and on response set up event listener
+}
+
+function onDataFinish() {
+	//this gets called when all of the historical data is loaded
+	//let's plot it!
+	var c = 0;
+	var messageLength = all_data.length;
+	for (c = 0; c < messageLength; c++){
+		sid = all_data[c].id;
+		if ( c == 0) {
+			processInitialDataChart("pm", 1, all_data[c], sid);
+		} else {
+			processStreamDataChart("pm", all_data[c], sid);
+		}
+	}
+}
+
+
+function setupLiveTokenListener() {
 	$.postJSON("https://api.opensensors.io/v1/login",
 		{
 			"username" : "solentairwatch",
-			"password" : prompt("Enter opensensors password:", "")
+			"password" : password
 		},
 		function(data, status) {
+			console.log("new token received");
 			token = data.token;	
-			console.log(status);
+
 			var apiRealtimeUrl = "https://realtime.opensensors.io/v1/events/users/solentairwatch/topics?token=" + token;
 			var eventUrl = new EventSource(apiRealtimeUrl);
+			//get a new token after a while
+			setTimeout(function(){
+				eventUrl.close();
+				console.log("getting new token");
+				setupLiveTokenListener();
+			}, 30*60*1000);
+			//the event listener
 			eventUrl.onmessage = function(event) {
 				eventnum += 1;
 				//how many of the inital messages do we ignore
-				var ignore = 3;
+				var ignore = 2;
 				var message = JSON.parse(JSON.parse(event.data).message);
-			//	console.log(message);
+				//	console.log(message);
 				var sid = message.id;
-				console.log("message from id " + sid);
+				//console.log("message from id " + sid);
 				if ( eventnum < ignore + 1 ) {
 					//opensensors sends strange initial data, dump it
 				} else if ( eventnum == ignore + 1){
 					//log the first one we're plotting
-					console.log(message);
+					//console.log(message);
 					processInitialDataChart("pm", 1, message, sid);
 				} else {
 					//update data
@@ -193,23 +268,24 @@ if(plot_mode == "H" || plot_mode == "h") {
 			}
 		}
 	);
+
 }
 
 function processInitialDataChart(sensor_name, chartid, message, sid) {
 	var dataXy = [{x: message.time, y: message.PM25}];
-	//	console.log(dataXy)
+	//console.log(dataXy)
 	var dataset = getOrCreateDataset(sensor_name, sid, chartid, "PM25");
 	dataset.data = dataXy;
 
 	// update all charts
-	//pmChartBySid[chartid].chart.update(00, false);
+	pmChartBySid[chartid].chart.update(00, false);
 }
 
 function processStreamDataChart(sensor_name, message, sid) {
 	getOrCreateDataset(sensor_name, sid, 1, "PM25");
 	var datapoint = {x: message.time, y: message.PM25};
 	var datapointWithID = {id: sid, datapoint : datapoint};
-//	addDataPoint(sid, 1, "PM25", datapoint);
+	//	addDataPoint(sid, 1, "PM25", datapoint);
 	addDataPointWithID(1, "PM25", datapointWithID);
 }
 
@@ -225,42 +301,22 @@ function addDataPointWithID(chartid, readingName, IDpoint) {
 	IDrequestRender(cfg);
 }
 
-function new_requestRender(sid, cfg) {
-	// update all charts
-
-	var chart = cfg.chart;
-
-	var buffer = cfg.buffer;
-	var dataset = chart.data.datasets[datasets_existing_for.indexOf(sid)];
-
-	var data = dataset.data;
-	data.push(buffer[0]);
-
-	buffer.length = 0;
-	if (data.length > maxDataPointsInCharts) {
-		data = data.slice(data.length - maxDataPointsInCharts);
-	}
-
-	dataset.data = data;
-
-	chart.update(0, false);
-}
-
 function IDrequestRender(cfg) {
 	// update all charts
 	if (cfg.timeout !== null) {
+		//console.log("updating already!");
 		// do nothing, there's a timeout already
 		return
 	}
 
 	// there is no timeout, so set one
 	cfg.timeout = setTimeout(function(){
-		//console.log("rendering", cfg.name, cfg);
+		//console.log("rendering");
 
 		var chart = cfg.chart;
 
 		var IDbuffer = cfg.IDbuffer;
-		
+
 		//var dataset = chart.data.datasets[datasets_existing_for.indexOf(sid)];
 
 		var datasets = chart.data.datasets;
@@ -288,47 +344,14 @@ function IDrequestRender(cfg) {
 	}, 500);
 }
 
-function requestRender(sid, cfg) {
-	// update all charts
-	if (cfg.timeout !== null) {
-		// do nothing, there's a timeout already
-		return
-	}
-
-	// there is no timeout, so set one
-	cfg.timeout = setTimeout(function(){
-		//console.log("rendering", cfg.name, cfg);
-
-		var chart = cfg.chart;
-
-		var buffer = cfg.buffer;
-		var dataset = chart.data.datasets[datasets_existing_for.indexOf(sid)];
-
-		var data = dataset.data.slice();
-		for (var j in buffer) {
-			data.push(buffer[j]);
-		}
-
-		buffer.length = 0;
-		if (data.length > maxDataPointsInCharts) {
-			data = data.slice(data.length - maxDataPointsInCharts);
-		}
-
-		dataset.data = data;
-
-		chart.update(0, false);
-		cfg.timeout = null;
-
-	}, 500);
-}
-
 //an array to keep track of which sensors have been given a dataset yet.
 //appended to in the order in which we create datasets!
 var datasets_existing_for = []; 
 // chart is a chart object
 // sensor_id is the id of the sensor we want to plot
 function getOrCreateDataset(sensor_name, sensor_id, chartid, stream_name) {
-	var cfg = pmChartBySid[chartid]
+	var cfg = pmChartBySid[chartid];
+	//console.log(cfg);
 	var chart = cfg.chart;
 	if (datasets_existing_for.includes(sensor_id)) {
 		return chart.data.datasets[datasets_existing_for.indexOf(sensor_id)];
